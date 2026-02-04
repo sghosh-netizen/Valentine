46257028e7fa7ef75884fe88cfcc86cc0def1ccf
sghosh-netizen <sghosh0999@gmail.com>
2026-02-05 01:42:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,27 +14,8 @@ function yesClick() {
         You just made me incredibly happy 🥰<br>
         I can't wait to make more memories with you.
       </p>
-      <img src="image/together.jpg" alt="us together">
+      <img src="image/PXL_20241019_100958843.RAW-01.COVER.jpg" alt="us together">
       <h2>It's a date 😍</h2>
     </div>
   `;
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-  const heartsContainer = document.querySelector(".hearts");
-
-  for (let i = 0; i < 20; i++) {
-    const heart = document.createElement("div");
-    heart.className = "heart";
-
-    const size = 15 + Math.random() * 20;
-    heart.style.width = size + "px";
-    heart.style.height = size + "px";
-
-    heart.style.left = Math.random() * 100 + "vw";
-    heart.style.animationDuration = 5 + Math.random() * 5 + "s";
-    heart.style.opacity = Math.random();
-
-    heartsContainer.appendChild(heart);
-  }
-});
